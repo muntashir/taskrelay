53a2b49ffabe0c00ff9a345a668a7faa314ddec6
Muntashir <kazimuntashir@gmail.com>
2016-11-07 21:36:31 -0500

    Prepare for release

---
+++ python/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'taskrelay',
+    version = '0.1.0',
+    description = 'A library to run tasks on remote servers',
+    license = 'MIT',
+    url = 'https://github.com/muntashir/taskrelay',
+    author = 'https://github.com/muntashir',
+    packages = [''],
+    install_requires = ['websockets']
+)
